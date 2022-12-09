d30e5958d2e810808a0d159faa6701d40483f9ad
pgardner <ptg2@rice.edu>
2022-12-09 02:13:40 -0600

    semi functional

--- workout.py
+++ workout.py
@@ -1,62 +1,121 @@
+class class_erg:
+    def __init__(self, meters, split, SR, HR, time):
+        self.meters = meters
+        self.split = split
+        self.SR = SR
+        self.HR = HR
+        self.time = time
+    @classmethod
+    def from_input(cls):
+        return cls(
+            input('meters: '),
+            input('split: '), 
+            input('SR: '),
+            input('HR: '),
+            input('time: ')
+        )
 
-#the first user prompt after starting the code
-purpose = input("Update (1) or Examine (2)? ")
-if(purpose == 1):
-    exercise = input("Exercise? ")
-#list of names for known workouts, can grow
-known_workouts = ['Erg','erg','erging','Run','run','jog','Jog']
-class_known_workouts = ['Erg','erg','erging','Run','run','jog','Jog']
-
+class class_run:
+    def __init__(self, miles, time, HR, elevationChange):
+        self.miles = miles
+        self.time = time
+        self.HR = HR
+        self.elevationChange = elevationChange
+    @classmethod
+    def from_input(cls):
+        return cls(
+            input('miles: '),
+            input('time: '), 
+            input('HR: '),
+            input('elevationChange: ')
+        )
 
 
 def check_workouts(exercise_now):
-    #exit code for starting the known workout class, or adding a workout, or third option
-    if exercise_now in known_workouts:
-        #start to find the right class to prompt user inputs
+    sport = [key for key, v in dic_known_workouts.items() if exercise_now in v]
+    if sport:
         print("Known workout! Tell me more:")
-        #open the right csv file
-        #use the right class to gather info
-        return known_workouts.index(exercise)
+        return True
     else:
-        addOrnot = input("Unknown workout! Would you like to add? y/n")
-        if(addOrnot == y):
-            return 2
-            #return the exit code that starts the add workout program
-            #go through procedure of adding workout
+        addOrNot = input("Unknown workout! Would you like to add it? (y/n): ")
+        if(addOrNot == 'y'):
+            #function for creating workout class nd writing a csv for furture storage
+            new_workout_type
+            return True
         else:
-            return strange
-            #not sure what to do n the case where they dont add.
-            #maybe ask if they want to update with a different workout or examine, or leave.
-            #return the exit code that does this^
+            continueOrNot = input("Record another workout? (y/n): ")
+            if(continueOrNot == 'n'):
+                #some redundancies here with changing global to false and returnng false
+                global keepGoing
+                keepGoing = False
+                return False
+            else:
+                keepGoing = True
+                return True
 
-#def note
-#def last(workout, total/average/print, days, counts)
+
+def record_exercise(exercise_now):
+    sport = [key for key, v in dic_known_workouts.items() if exercise_now in v][0]
+    obj = classes_map[sport].from_input()
+    #add workout to the correct csv
+    store_workout
+    #add workout to tbeautiful text file
+    publish_workout
+
+def store_workout():
+    #publish to beautiful text file
+    #gonna be a function built on pandas
+    i = 123 
+def publish_workout():
+    #publish to beautiful text file
+    i = 123
+
+def new_workout_type():
+    #update the dictionary
+    #update the classes_map
+    #create a csv with proper columns
+    i = 123
+
+
+#list of names for known workouts, can grow
+dic_known_workouts = {
+    'dic_erg_names' : ['Erg','erg','erging','rower'],
+    'dic_run_names' : ['Run','run','jog','Jog']
+}
+classes_map = {'dic_erg_names': class_erg, 'dic_run_names': class_run}
+
+#the first user prompt after starting the code
+purpose = input('Update (U) or Examine (E)? ')
+
+if(purpose == 'U'):
+    keepGoing = True
+
+while(keepGoing == True):
+    exercise = input('Exercise? ')
+    if not check_workouts(exercise):
+        break
+    event = record_exercise(exercise)
 
 
 
-with open (saveAddr+".csv",'a'): 
-    month = data['month_number'].tolist()
+print("Go drink some water")
 
-with open('test.txt', 'r') as text:
-    lines = text.readlines()
-    for x in lines:
-        if x.startswith('Meters'):
-            vec_meters = text_file.read().split(',').
-    vec_split = text_file.read().split(',').
-    vec_strokeRate = text_file.read().split(',').
-    vec_HR = text_file.read().split(',').
-    vec_time = text_file.read().split(',').
 
-class erg:
-    def __init__(self, meters, split, strokeRate, HR, time):
-        self.meters = input("meters: ")
-        self.split = input("split: ")
-        self.strokeRate = input("strokeRate: ")
-        self.HR = input("HR: ")
-        self.time = input("time: ")
-    def append(self):
-        vec_meters.append(meters)
-        vec_split.append(split)
-        vec_strokeRate.append(strokeRate)
-        vec_HR.append(HR)
-        vec_time.append(time)
+
+# #def note
+# #def last(workout, total/average/print, days, counts)
+
+
+
+# with open (saveAddr+'.csv','a'): 
+#     month = data['month_number'].tolist()
+
+# with open('test.txt', 'r') as text:
+#     lines = text.readlines()
+#     for x in lines:
+#         if x.startswith('Meters'):
+#             vec_meters = text_file.read().split(',').
+#     vec_split = text_file.read().split(',').
+#     vec_strokeRate = text_file.read().split(',').
+#     vec_HR = text_file.read().split(',').
+#     vec_time = text_file.read().split(',').
